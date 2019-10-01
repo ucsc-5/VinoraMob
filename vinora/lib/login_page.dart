06c64f770e07a7795c6e6205774aa695c0ce407f
Chamod Lakmal <chamodlakmal97@gmail.com>
2019-10-01 08:47:33 +0530

    added fonts

--- vinora/lib/login_page.dart
+++ vinora/lib/login_page.dart
@@ -6,7 +6,7 @@ import 'package:toast/toast.dart';
 import 'package:connectivity/connectivity.dart';
 import 'data/food_data.dart';
 import 'package:progress_dialog/progress_dialog.dart';
-import 'data/food_data.dart';
+import 'package:tel_input/tel_input.dart';
 class LoginPage extends StatefulWidget{
   const LoginPage({this.onSignedIn});
   final VoidCallback onSignedIn;
@@ -107,6 +107,7 @@ class LoginPage extends StatefulWidget{
                     border: OutlineInputBorder(),
                     labelText: 'Enter Email', 
                   ),
+                  keyboardType: TextInputType.emailAddress,
                   validator: (value)=>value.isEmpty?"Email can't be Empty":null ,
                   onSaved: (value)=>_email=value,
                 ),
@@ -119,7 +120,29 @@ class LoginPage extends StatefulWidget{
                     border: OutlineInputBorder(),
                     labelText: 'Enter Mobile Number', 
                   ),
-                  validator: (value)=>value.isEmpty?"Mobile can't be Empty":null ,
+                  maxLength: 10,
+                  keyboardType: TextInputType.phone,
+                  onChanged:(value){
+                    if(value.length<10)
+                    {
+                      return "Phone number length not complete ";
+                    }else if(value.length==10){
+                      return "Phone number length is success";
+                    }else{
+                      return null;
+                    }
+                  } ,
+                  validator: (value){
+                    if(value.isEmpty){
+                      return "Mobile can't be Empty";
+                    }
+                    else if(value.length<10)
+                    {
+                      return "Please Enter Valid Phone number";
+                    }else{
+                      return  null;
+                    }
+                  },
                   onSaved: (value)=>_mobile=value,
                 ),
                 
@@ -196,7 +219,15 @@ class LoginPage extends StatefulWidget{
                                   }
                                   
                                 }
-                                                  
+                                bool validateMobile(String value) {
+                                  String patttern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
+                                  RegExp regExp = new RegExp(patttern);
+                                  
+                                   if (!regExp.hasMatch(value)) {
+                                        return false;
+                                  }
+                                  return true;
+                                  }                  
                                 bool validateAndSave() {
                                   final form =formKey.currentState;
                                   if(form.validate()){
@@ -270,7 +301,29 @@ class LoginPage extends StatefulWidget{
                    List<Widget> logo() {
                      AssetImage assetImage=AssetImage('images/logo1.png');
                      Image image=Image(image:assetImage,width: 150,height: 150);
-                 return[                  
+                 return[
+                   SizedBox(height:50.0),
+                   Text("VinoraMob",style: TextStyle(
+                     fontSize: 55.0,
+                     fontFamily: 'CONCERTONE',
+                     fontStyle: FontStyle.normal,
+                     color: Colors.blueGrey,
+                     letterSpacing: 2,
+                     shadows: [
+        
+        Shadow( // bottomRight
+          offset: Offset(1.5, -1.5),
+          color: Colors.black
+        ),
+        Shadow( // topRight
+          offset: Offset(1.5, 1.5),
+          color: Colors.deepOrange
+        ),
+        
+      ]
+    
+                     
+                   ),),                  
                     image
                    ];
                }
